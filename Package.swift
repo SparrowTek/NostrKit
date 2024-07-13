6fbe948e822b11f23179edf08a71836b68e2ec33
Thomas Rademaker <thomas@sparrowtek.com>
2024-07-13 17:06:21 -0400

    add CoreNostr dependency

--- Package.swift
+++ Package.swift
@@ -11,15 +11,13 @@ let package = Package(
             targets: ["NostrKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", from: "0.17.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.4.0"),
+        .package(url: "git@github.com:SparrowTek/CoreNostr.git", branch: "main"),
     ],
     targets: [
         .target(
             name: "NostrKit",
             dependencies: [
-                .product(name: "secp256k1", package: "secp256k1.swift"),
-                .product(name: "Crypto", package: "swift-crypto"),
+                "CoreNostr"
             ]),
         .testTarget(
             name: "NostrKitTests",
