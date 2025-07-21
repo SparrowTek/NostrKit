8178a97364eb48528fc88cdb8e1b49917ba9f6f5
Thomas Rademaker <thomas@radmakr.com>
2025-07-21 15:18:57 -0400

    everything is local to this repo.. for now

--- Package.swift
+++ Package.swift
@@ -4,7 +4,13 @@ import PackageDescription
 
 let package = Package(
     name: "NostrKit",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
         .library(
             name: "NostrKit",
@@ -12,7 +18,14 @@ let package = Package(
     ],
     dependencies: [
 //        .package(url: "git@github.com:SparrowTek/CoreNostr.git", branch: "main"),
-        .package(path: "../CoreNostr")
+        
+        .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1", from: "0.21.1"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.4.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.9.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.6.0"),
+        .package(url: "https://github.com/bitcoindevkit/bdk-swift.git", from: "2.0.0"),
+        .package(url: "https://github.com/valpackett/SwiftCBOR.git", from: "0.5.0"),
+        .package(url: "https://github.com/SparrowTek/Vault.git", from: "1.0.0"),
     ],
     targets: [
         .target(
@@ -20,6 +33,18 @@ let package = Package(
             dependencies: [
                 "CoreNostr"
             ]),
+        .target(
+            name: "CoreNostr",
+            dependencies: [
+                .product(name: "P256K", package: "swift-secp256k1"),
+                .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "CryptoSwift", package: "CryptoSwift"),
+                .product(name: "BigInt", package: "BigInt"),
+                .product(name: "BitcoinDevKit", package: "bdk-swift"),
+                .product(name: "SwiftCBOR", package: "SwiftCBOR"),
+                .product(name: "Vault", package: "Vault"),
+            ]
+        ),
         .testTarget(
             name: "NostrKitTests",
             dependencies: ["NostrKit"]
