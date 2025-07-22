5749c9e2c80eb172afb5f30969b4a6b8cb2db3a3
Thomas Rademaker <thomas@radmakr.com>
2025-07-22 15:19:55 -0400

    pull in CoreNostr via git

--- Package.swift
+++ Package.swift
@@ -17,15 +17,7 @@ let package = Package(
             targets: ["NostrKit"]),
     ],
     dependencies: [
-//        .package(url: "git@github.com:SparrowTek/CoreNostr.git", branch: "main"),
-        
-        .package(url: "https://github.com/21-DOT-DEV/swift-secp256k1", from: "0.21.1"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.4.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.9.0"),
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.6.0"),
-        .package(url: "https://github.com/bitcoindevkit/bdk-swift.git", from: "2.0.0"),
-        .package(url: "https://github.com/valpackett/SwiftCBOR.git", from: "0.5.0"),
-        .package(url: "https://github.com/SparrowTek/Vault.git", from: "1.0.0"),
+        .package(url: "git@github.com:SparrowTek/CoreNostr.git", branch: "main"),
     ],
     targets: [
         .target(
@@ -33,18 +25,6 @@ let package = Package(
             dependencies: [
                 "CoreNostr"
             ]),
-        .target(
-            name: "CoreNostr",
-            dependencies: [
-                .product(name: "P256K", package: "swift-secp256k1"),
-                .product(name: "Crypto", package: "swift-crypto"),
-                .product(name: "CryptoSwift", package: "CryptoSwift"),
-                .product(name: "BigInt", package: "BigInt"),
-                .product(name: "BitcoinDevKit", package: "bdk-swift"),
-                .product(name: "SwiftCBOR", package: "SwiftCBOR"),
-                .product(name: "Vault", package: "Vault"),
-            ]
-        ),
         .testTarget(
             name: "NostrKitTests",
             dependencies: ["NostrKit"]
